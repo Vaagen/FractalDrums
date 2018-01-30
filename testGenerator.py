ac381d5f97c794b741ebc2a87cdbf1a862645c7f
Vaagen <pcthrane@stud.ntnu.no>
2018-01-30 12:19:29 +0100

    rewriting testGenerator as a function

--- testGenerator.py
+++ testGenerator.py
@@ -5,51 +5,60 @@ def pause(message):
     print(message)
     programPause = raw_input("Press <ENTER> to continue...")
 
-case =3
-if case==0:
-    Rxx=1
-    Ryy=1
-    Rxy=0
-    Ryx=0
-elif case==1:
-    Rxx=0
-    Ryy=0
-    Rxy=-1
-    Ryx=1
-elif case==2:
-    Rxx=-1
-    Ryy=-1
-    Rxy=0
-    Ryx=0
-else:
-    Rxx=0
-    Ryy=0
-    Rxy=1
-    Ryx=-1
+def generator(coords, Ip0, Ip7):
+    (x0,y0)=coords[Ip0,:]
+    (x7,y7)=coords[Ip7,:]
+
+    if   x0<x7  and y0==y7:
+        delta=(x7-x0)/4
+        print delta
+        Rxx=1
+        Ryy=1
+        Rxy=0
+        Ryx=0
+    elif x0==x7 and y0>y7:
+        delta=(y0-y7)/4
+        Rxx=0
+        Ryy=0
+        Rxy=-1
+        Ryx=1
+    elif x0>x7  and y0==y7:
+        delta=(x0-x7)/4
+        Rxx=-1
+        Ryy=-1
+        Rxy=0
+        Ryx=0
+    elif x0==x7 and y0<y7:
+        delta=(y7-y0)/4
+        Rxx=0
+        Ryy=0
+        Rxy=1
+        Ryx=-1
+    else:
+        print('Error: Unexpected coordinates recieved in generator.')
+
+    deltaIndex = (Ip7-Ip0)/7
+    coords[Ip0+  deltaIndex,0] = x0+  Rxx*delta
+    coords[Ip0+2*deltaIndex,0] = x0+  Rxx*delta+Ryx*delta
+    coords[Ip0+3*deltaIndex,0] = x0+2*Rxx*delta+Ryx*delta
+    coords[Ip0+4*deltaIndex,0] = x0+2*Rxx*delta-Ryx*delta
+    coords[Ip0+5*deltaIndex,0] = x0+3*Rxx*delta-Ryx*delta
+    coords[Ip0+6*deltaIndex,0] = x0+3*Rxx*delta
+
+    coords[Ip0+  deltaIndex,1] = y0          +  Rxy*delta
+    coords[Ip0+2*deltaIndex,1] = y0+Ryy*delta+  Rxy*delta
+    coords[Ip0+3*deltaIndex,1] = y0+Ryy*delta+2*Rxy*delta
+    coords[Ip0+4*deltaIndex,1] = y0-Ryy*delta+2*Rxy*delta
+    coords[Ip0+5*deltaIndex,1] = y0-Ryy*delta+3*Rxy*delta
+    coords[Ip0+6*deltaIndex,1] = y0          +3*Rxy*delta
 
 
-delta=1
-x0=3
-y0=5
 
 coords = np.zeros((8,2))
-coords[0,0] = x0
-coords[1,0] = x0+  Rxx*delta
-coords[2,0] = x0+  Rxx*delta+Ryx*delta
-coords[3,0] = x0+2*Rxx*delta+Ryx*delta
-coords[4,0] = x0+2*Rxx*delta-Ryx*delta
-coords[5,0] = x0+3*Rxx      -Ryx*delta
-coords[6,0] = x0+3*Rxx
-coords[7,0] = x0+4*Rxx
+coords[0,:]=[1,2]
+coords[7,:]=[8,2]
+generator(coords,0,7)
 
-coords[0,1] = y0
-coords[1,1] = y0          +  Rxy*delta
-coords[2,1] = y0+Ryy*delta+  Rxy*delta
-coords[3,1] = y0+Ryy*delta+2*Rxy*delta
-coords[4,1] = y0-Ryy*delta+2*Rxy*delta
-coords[5,1] = y0-Ryy*delta+3*Rxy*delta
-coords[6,1] = y0          +3*Rxy*delta
-coords[7,1] = y0          +4*Rxy*delta
 
 fig =plt.figure()
 ax = fig.add_subplot(111)
