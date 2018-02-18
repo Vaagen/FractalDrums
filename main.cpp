23322d7239f8e58039b7e867d0f87e259aa14388
Vaagen <pcthrane@stud.ntnu.no>
2018-02-18 08:39:51 +0100

    Add working test of making sparse T and A.

--- main.cpp
+++ main.cpp
@@ -29,19 +29,38 @@ int main(int argc, char *argv[]){
   }
 
   // Test making of sparse matrices needed.
-  int N = 7;
-  arma::sp_mat testSparse(N,N);
-  testSparse.speye();
-  testSparse*=4;
-  testSparse.print();
+  int N = goalLevel;
+  arma::sp_mat sparseT(N-1,N-1);
+  sparseT.speye();
+  sparseT*=4;
+  //sparseT.print();
 
-  testSparse(0,1)=-1;
-  for(int i=1; i<(testSparse.n_rows-1); i++){
-    std::cout << i << testSparse.n_rows << std::endl;
-    testSparse(i,i-1)=testSparse(i,i+1)=-1;
+  sparseT(0,1)=-1;
+  for(int i=1; i<(sparseT.n_rows-1); i++){
+    sparseT(i,i-1)=sparseT(i,i+1)=-1;
   }
-  testSparse(testSparse.n_rows-1,testSparse.n_rows-2)=-1;
-  testSparse.print();
+  sparseT(sparseT.n_rows-1,sparseT.n_rows-2)=-1;
+  //sparseT.print();
+
+  arma::sp_mat sparseIdentity(N-1,N-1);
+  sparseIdentity.speye();
+  //sparseIdentity.print();
+
+  arma::sp_mat sparseA((N-1)*N,(N-1)*N);
+  sparseA.rows(0,(N-2)).cols(0,N-2)=sparseT;
+  sparseA.rows(0,(N-2)).cols(N-1,2*(N-1)-1)=-sparseIdentity;
+  for(int i=1; i<(N-1); i++){
+    sparseA.rows(i*(N-1),(i+1)*(N-1)-1).cols((i-1)*(N-1),i*(N-1)-1)=-sparseIdentity;
+    sparseA.rows(i*(N-1),(i+1)*(N-1)-1).cols(i*(N-1),(i+1)*(N-1)-1)=sparseT;
+    sparseA.rows(i*(N-1),(i+1)*(N-1)-1).cols((i+1)*(N-1),(i+2)*(N-1)-1)=-sparseIdentity;
+  }
+  sparseA.rows((N-1)*(N-1),N*(N-1)-1).cols((N-2)*(N-1),(N-1)*(N-1)-1)=-sparseIdentity;
+  sparseA.rows((N-1)*(N-1),N*(N-1)-1).cols((N-1)*(N-1),N*(N-1)-1)=sparseT;
+  //sparseA.print();
+
+  // Now, how to use the grid as a column vector? Note, it's a dense matrix.
+
+  return 0;
 
   // Calculating corners.
   arma::mat coords;
