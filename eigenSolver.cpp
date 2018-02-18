c3b24b0910ea3ef625eea678bf8094df2a3b89f5
Vaagen <pcthrane@stud.ntnu.no>
2018-02-18 23:10:33 +0100

    Fix getHelmholtzMatrix to reflect correct boundary conditions.

--- eigenSolver.cpp
+++ eigenSolver.cpp
@@ -1,7 +1,7 @@
 #include "eigenSolver.h"
 
 void eigenSolver::getHelmholtzMatrix(arma::sp_mat& sparseA, int N){
-  arma::sp_mat sparseT(N-1,N-1);
+  arma::sp_mat sparseT(N,N);
   sparseT.speye();
   sparseT*=4;
 
@@ -11,17 +11,17 @@ void eigenSolver::getHelmholtzMatrix(arma::sp_mat& sparseA, int N){
   }
   sparseT(sparseT.n_rows-1,sparseT.n_rows-2)=-1;
 
-  arma::sp_mat sparseIdentity(N-1,N-1);
+  arma::sp_mat sparseIdentity(N,N);
   sparseIdentity.speye();
 
-  sparseA = arma::resize(sparseA,(N-1)*N,(N-1)*N);
-  sparseA.rows(0,(N-2)).cols(0,N-2)=sparseT;
-  sparseA.rows(0,(N-2)).cols(N-1,2*(N-1)-1)=-sparseIdentity;
+  sparseA = arma::resize(sparseA,N*N,N*N);
+  sparseA.rows(0,(N-1)).cols(0,N-1)=sparseT;
+  sparseA.rows(0,(N-1)).cols(N,2*N-1)=-sparseIdentity;
   for(int i=1; i<(N-1); i++){
-    sparseA.rows(i*(N-1),(i+1)*(N-1)-1).cols((i-1)*(N-1),i*(N-1)-1)=-sparseIdentity;
-    sparseA.rows(i*(N-1),(i+1)*(N-1)-1).cols(i*(N-1),(i+1)*(N-1)-1)=sparseT;
-    sparseA.rows(i*(N-1),(i+1)*(N-1)-1).cols((i+1)*(N-1),(i+2)*(N-1)-1)=-sparseIdentity;
+    sparseA.rows(i*N,(i+1)*N-1).cols((i-1)*N,i*N-1)=-sparseIdentity;
+    sparseA.rows(i*N,(i+1)*N-1).cols(i*N,(i+1)*N-1)=sparseT;
+    sparseA.rows(i*N,(i+1)*N-1).cols((i+1)*N,(i+2)*N-1)=-sparseIdentity;
   }
-  sparseA.rows((N-1)*(N-1),N*(N-1)-1).cols((N-2)*(N-1),(N-1)*(N-1)-1)=-sparseIdentity;
-  sparseA.rows((N-1)*(N-1),N*(N-1)-1).cols((N-1)*(N-1),N*(N-1)-1)=sparseT;
+  sparseA.rows(N*(N-1),N*N-1).cols(N*(N-2),N*(N-1)-1)=-sparseIdentity;
+  sparseA.rows(N*(N-1),N*N-1).cols(N*(N-1),N*N-1)=sparseT;
 }
