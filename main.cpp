2dccaa4371cc9e8a25a2fb6a46dfc44e2827f236
Vaagen <pcthrane@stud.ntnu.no>
2018-02-19 21:15:35 +0100

    Add timer function.

--- main.cpp
+++ main.cpp
@@ -3,8 +3,17 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <ctime>
+
+
+void printTime(int start_s){
+  int stop_s=clock();
+  std::cout << "    Seconds since start: " << (stop_s-start_s)/double(CLOCKS_PER_SEC) << std::endl;
+}
 
 int main(int argc, char *argv[]){
+  // Starting timer.
+  int start_s=clock();
   // Getting goalLevel.
   int goalLevel;
   if (argc <2){
